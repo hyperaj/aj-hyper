622c4152ad6c7efc2df4857de29c843b78499084
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-05-03 01:59:49 +0530

    Update userbot.py

--- AM/core/userbot.py
+++ AM/core/userbot.py
@@ -13,6 +13,7 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
+            name="hyper",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_name=str(config.STRING1),
